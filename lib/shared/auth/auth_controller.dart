e9d58687c415711bfb521a2bac9d8c22ddda4122
Lucas <lucas.htferreira@outlook.com>
2021-06-24 17:02:15 -0300

    Added auto login

--- lib/shared/auth/auth_controller.dart
+++ lib/shared/auth/auth_controller.dart
@@ -9,6 +9,7 @@ class AuthController {
 
   void setUser(BuildContext context, UserModel? user) {
     if (user != null) {
+      saveUser(user);
       _user = user;
       Navigator.pushReplacementNamed(context, "/home");
     } else {
@@ -18,11 +19,12 @@ class AuthController {
 
   Future<void> saveUser(UserModel user) async {
     final instance = await SharedPreferences.getInstance();
-    await instance.setString("user", user as String);
+    await instance.setString("user", user.toJson());
   }
 
   Future<void> currentUser(BuildContext context) async {
     final instance = await SharedPreferences.getInstance();
+    await Future.delayed(Duration(seconds: 2));
     if (instance.containsKey("user")) {
       final json = instance.get("user") as String;
       setUser(context, UserModel.fromJson(json));
